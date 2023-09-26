645a12118ce64283c2ad16fbfd6650ffc7c00f69
wiwianquek <136752154+wiwianquek@users.noreply.github.com>
2023-09-27 00:30:34 +0800

    draft 1
    added the full rough pic of the typeracer

--- setup/main.js
+++ setup/main.js
@@ -1,18 +1,75 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random' //to get a random quote from this API
-const quoteDisplayElement = document.getElementById('quoteDisplay')
+document.addEventListener('DOMContentLoaded', function() {
+    const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
+    const quoteDisplay = document.querySelector('.quote-display');
+    const quoteInput = document.querySelector('.quote-input');
+    const timer = document.querySelector('.timer');
+    let correct = false; // Initialize correct variable
 
-function getRandomQuote() {
-    return fetch(RANDOM_QUOTE_API_URL)
-        .then(response => response.json())
-        .then(data => data.content) //to get the content in the api 
-}
+//We wanna add eventlistener here whenever something is typed in the quote-input section, we will use the 'input' event to listen for changes in the value of the quoteInput element.
+   
+    quoteInput.addEventListener('input', function() {
+        const arrayQuote = quoteDisplay.querySelectorAll('span');
+        const arrayValue = quoteInput.value.split('');
+        arrayQuote.forEach((alphabetSpan, index) => {
+            const alphabet = arrayValue[index];
+            if (alphabet === '') {
+                alphabetSpan.classList.remove('correct');
+                alphabetSpan.classList.remove('incorrect');
+            } else if (alphabet === alphabetSpan.innerText) {
+                alphabetSpan.classList.add('correct');
+                alphabetSpan.classList.remove('incorrect');
+            } else {
+                alphabetSpan.classList.remove('correct');
+                alphabetSpan.classList.add('incorrect');
+                correct = false;
+            }
+        });
+    });
 
+    async function getRandomQuote() {
+        try {
+            let res = await fetch(RANDOM_QUOTE_API_URL); //fetch the quote from the api
+            if (res.status === 200) {
+                let data = await res.json(); //this only stores the quote we want 
+                let quote = data['content'];
+                return quote;
+            } else {
+                throw new Error('Failed to fetch a random quote'); //throws a proper error instead of using "return res.json;"
+            }
+        } catch (error) {
+            console.error(error);
+            return '';
+        }
+    }
 
-async function renderNewQuote() { 
-    const quote = await getRandomQuote() //await is used to wait for the getRandomQuote function to complete and return a result before proceeding 
-    quoteDisplayElement.innerText = quote
-}
+    function clearQuoteDisplay() {
+        quoteDisplay.innerText = ''; //displays api quote on the quote-display section 
+    }
 
+    function renderQuote(quote) {
+        quote.split('').forEach(alphabet => { //we will split each individual characters in the quote here
+            const alphabetSpan = document.createElement('span');
+            alphabetSpan.classList.add('correct');
+            alphabetSpan.innerText = alphabet;
+            quoteDisplay.appendChild(alphabetSpan);
+        });
+    }
 
-renderNewQuote()
+    function startTimer() {
+        let startTime = Date.now();
+        setInterval(() => {
+            const elapsedSeconds = Math.floor((Date.now() - startTime) / 1000);
+            timer.innerText = elapsedSeconds;
+        }, 1000);
+    }
 
+    async function renderNewQuote() {
+        clearQuoteDisplay();
+        const quote = await getRandomQuote();
+        renderQuote(quote);
+        startTimer();
+        correct = true; // Set correct to true when a new quote is rendered
+    }
+
+    renderNewQuote();
+});
